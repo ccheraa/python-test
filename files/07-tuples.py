575b6c6e254873af8a7237dbe5e8af50886cdc19
Charafeddine Cheraa <ccheraa@gmail.com>
2020-05-25 22:30:58 +0100

    tuples

---
+++ files/07-tuples.py
@@ -0,0 +1,15 @@
+l1 = ('apple', 'pineapple', 'banana')
+l2 = ('japan', 'egypt', 'spain')
+l3 = l1 + l2
+print('l1:', l1)
+print('l2:', l2)
+print('l3:', l3)
+print('l1[1]:', l1[1])
+print('l1[-1]:', l1[-1])
+print('l3[2:-1]', l3[2:-1])
+print('l3:')
+for x in l3:
+  print('  -', x)
+print('japan in l1:', 'japan' in l1)
+print('japan in l3:', 'japan' in l3)
+print('len(l3):', len(l3))
